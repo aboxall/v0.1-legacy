176132f006252db9c5d066dc3716015b60a35819
Adam Boxall <adam.boxall@uk.insight.com>
2010-03-16 11:11:06 +0000

    Implemented config library

--- system/library/Config.php
+++ system/library/Config.php
@@ -2,36 +2,90 @@
 
 class Config
 {
-    private $properties = array();
+    public $properties = array();
+    public $path;
 
     public function __construct()
     {
-		require_once 'config.conf';
+        $this->path = APP_PATH . '/configs/';
 
-		$this->properties = $config;
+        // default config files to load
+        $this->parse('config.ini', true);
+        $this->parse('database.ini', true);
     }
 
-    public function get($name)
+    public function parse($ini, $file = false, $return = false)
     {
-		if (isset($this->properties[$name]))
-		{
-        	return $this->properties[$name];
-		}
+        $path = $ini;
+
+        if ($file && !file_exists($path))
+        {
+            $path = $this->path . $ini;
+        }
+
+        $parsed = $file ? parse_ini_file($path, true) : parse_ini_string($ini, true);
+
+        if ($return)
+        {
+            return $parsed;
+        }
+
+        foreach ($parsed as $section => $properties)
+        {
+            $this->properties[$section] = $properties;
+        }
     }
 
-	public function set($name, $value)
+    public function get($property)
+    {
+        $tokens = explode('.', $property);
+        $return = $this->properties;
+
+        foreach ($tokens as $token)
+        {
+            $return = $this->recurseGet($token, $return);
+
+            if (!$return)
+            {
+                break;
+            }
+        }
+
+        return $return;
+    }
+
+	public function set($property, $value)
 	{
-		$this->properties[$name] = $value;
+        // FIXME - got frustrated and did this as a temp fix
+
+        $tokens = explode('.', $property);
+        $count  = count($tokens);
+
+        switch ($count)
+        {
+            case 1:
+                $this->properties[$property] = $value;
+                break;
+            case 2:
+                $this->properties[$tokens[0]][$tokens[1]] = $value;
+                break;
+            case 3:
+                $this->properties[$tokens[0]][$tokens[1]][$tokens[2]] = $value;
+                break;
+        }
+
+        return true;
 	}
 
-	public function rem($name)
-	{
-		if (isset($this->properties[$name]))
-		{
-			unset($this->properties[$name]);
-		}
-	}
+    private function recurseGet($token, $properties)
+    {
+        if (array_key_exists($token, $properties))
+        {
+            return $properties[$token];
+        }
 
+        return false;
+    }
 }
 
 // EOF
