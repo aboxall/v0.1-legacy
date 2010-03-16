952e2995384ece0e00ce4f0a4252356ec004ebc5
Bogdan Olteanu <olteanu.bogdan@gmail.com>
2010-03-16 16:19:58 +0200

    changed the ext of files from global

---
+++ system/application/views/global/head.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<title>CompanyName - PageName</title>
+<meta http-equiv="Content-Language" content="en-us" />
+<meta http-equiv="imagetoolbar" content="no" />
+<meta name="MSSmartTagsPreventParsing" content="true" />
+<meta name="description" content="Description" />
+<meta name="keywords" content="Keywords" />
+<meta name="author" content="A&amp;B Designs" />
+<style type="text/css" media="all">
+/*<![CDATA[*/
+@import "css/master.css";
+/*]]>*/
+</style>
+</head>
--- system/application/views/global/head.tpl
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<title>{$page_title|default:"Welcome to LivePHP"} - livephp.net</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="description" content="{$page_description|default:"default description of livephp here"}" />
-	<meta name="keywords" content="{$page_keywords|default:"livephp, live, php, help, tutorials, resources, programming"}" />
-</head>
