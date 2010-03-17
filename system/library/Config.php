a9de4070a9b50d02be87ec7333fd5ab214a97aac
Adam Boxall <adam.boxall@uk.insight.com>
2010-03-17 10:43:46 +0000

    Improved Config functionality and comments

--- system/library/Config.php
+++ system/library/Config.php
@@ -19,29 +19,57 @@ class Config
 
     private function __construct()
     {
+        // create a local var for the configs dir path
         $this->path = APP_PATH . '/configs/';
 
         // default config files to load
         $this->parse('config.ini', true);
         $this->parse('database.ini', true);
+
+        // check whether we're using enviroment levels
+        if ($this->get('server.use_enviroment'))
+        {
+            // set the dev/prod enviroment
+            $this->setEnviroment();
+        }
     }
 
     public function parse($ini, $file = false, $return = false)
     {
-        $path = $ini;
-
-        if ($file && !file_exists($path))
+        // if $ini is a file..
+        if ($file)
         {
-            $path = $this->path . $ini;
+            // preserve the original $ini var for later
+            $path = $ini;
+
+            // check if $ini param contains the relative/absolute
+            // path to the config file by seeing if it exists
+            if (!file_exists($path))
+            {
+                // if it doesn't prepend the default config path
+                $path = $this->path . $path;
+
+                // make sure the config file now exists
+                if (!file_exists($path))
+                {
+                    // if not throw a Config exception
+                    throw new ConfigException('Config file doesn\'t exist: ' . $ini);
+                }
+            }
         }
 
-        $parsed = $file ? parse_ini_file($path, true) : parse_ini_string($ini, true);
+        // if $file is true we need to parse $ini as 
+        $parsed = $file ? parse_ini_file($ini, true) : parse_ini_string($ini, true);
 
+        // if the return param is set we want to return the parsed
+        // INI file instead of adding to the properties array
         if ($return)
         {
             return $parsed;
         }
 
+        // loop through the parsed array and add
+        // each section to the properties array
         foreach ($parsed as $section => $properties)
         {
             $this->properties[$section] = $properties;
@@ -97,6 +125,56 @@ class Config
         }
 
         return false;
+    }
+
+    private function setEnviroment($overwrite = false, $debug_mode = false)
+    {
+        if (!empty($overwrite))
+        {
+            // if a custom enviroment has been passed or we're overwriting
+            // the default handling of dev/prod, set the enviroment to that 
+            $env  = $overwrite;
+        }
+        elseif ($_SERVER['SERVER_NAME'] == $this->get('server.prod_domain'))
+        {
+            // we're on the production server
+            $env = 'prod';
+        }
+        else
+        {
+            // default to a dev enviroment
+            $env = 'dev';
+            
+            // debug mode should always be enabled on dev
+            $debug_mode = true;
+        }
+
+        // set the server enviroment property
+        $this->set('server.enviroment', $env);
+
+        // set the debug mode property
+        $this->set('server.debug_mode', $debug_mode);
+
+        // write debug log
+        Logger::write('Enviroment set: ' . $env);
+    }
+
+    public function getEnviroment()
+    {
+        // returns the enviroment string (by default dev or prod)
+        return $this->get('server.enviroment');
+    }
+
+    public function isDev()
+    {
+        // return true if we're in the dev enviroment
+        return ($this->get('server.enviroment') == 'dev') ? true : false;
+    }
+
+    public function isProd()
+    {
+        // return true if we're in the prod enviroment
+        return ($this->get('server.enviroment') == 'prod') ? true : false;
     }
 }
 
